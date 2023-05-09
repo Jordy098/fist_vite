0e68d934c8ae26752ffee2a5908d3f458ee0b9d8
Jordy Perez <jordy@pop-os.localdomain>
2023-05-08 23:23:46 -0400

    first commit

---
+++ src/components/ButtonCount.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonCount = ({count,setCount}) => {
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+    </button>
+  )
+}
+
+export default ButtonCount
